ebf6b75d47bc37f0c30189ce1b6354103d03e3aa
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2024-05-09 12:06:56 +0200

    Ajout de 3 positions pour les entrainement thématiques.

--- app/select-thematic-training/page.tsx
+++ app/select-thematic-training/page.tsx
@@ -82,6 +82,13 @@ const SelectThematicTrainingPage = () => {
             playerColor: 'b',
             avantage: 'medium'
         },
+        {
+            title: 'Ruy Lopez Knight Journey',
+            startingFen: 'r2q1rk1/4bppp/p1np1n2/1pp1p2b/4P3/2PP1NNP/PPB2PP1/R1BQR1K1 b - - 3 14',
+            nextMove: 'Bg6',
+            playerColor: 'w',
+            avantage: 'low'
+        },
     ];
 
     const defensePositions = [
@@ -106,11 +113,18 @@ const SelectThematicTrainingPage = () => {
             playerColor: 'b',
             avantage: 'low'
         },
+        {
+            title: 'sicilian Defense',
+            startingFen: 'r1b1kbnr/3n1ppp/p7/1p6/3BP3/2N3P1/PPP2P1P/R3KB1R w KQkq - 0 12',
+            nextMove: 'Nd5',
+            playerColor: 'b',
+            avantage: 'low'
+        },
     ];
 
     const endgamePositions = [
         {
-            title: 'Finale de pions n°1',
+            title: 'Finale de pions',
             startingFen: '8/8/1p1p1k2/3P4/3K4/P7/8/8 b - - 5 47',
             nextMove: 'Ke7',
             playerColor: 'w',
@@ -124,53 +138,60 @@ const SelectThematicTrainingPage = () => {
             avantage: 'high'
         },
         {
-            title: 'Finale fou + tour contre tour',
+            title: 'Finale des tours',
             startingFen: '1r6/5p2/6pp/1b2k3/3RP3/4KP1P/6P1/8 w - - 4 37',
             nextMove: 'Rb4',
             playerColor: 'b',
             avantage: 'medium'
         },
         {
-            title: 'Finale de tours n°1',
+            title: 'Finale de tours',
             startingFen: '3r4/5pkp/4p1p1/1R6/3p1P2/8/P2K1P1P/8 w - - 0 24',
             nextMove: 'Kd3',
             playerColor: 'b',
             avantage: 'medium'
         },
         {
-            title: 'Finale de tours n°2',
+            title: 'Finale de tours',
             startingFen: '5R2/8/r3k3/5p2/4p1p1/6P1/4K3/8 w - - 1 52',
             nextMove: 'Ke3',
             playerColor: 'b',
             avantage: 'high'
         },
         {
-            title: 'Pion passé n°1',
+            title: 'Pion passé',
             startingFen: '1r2k3/p1P1b2p/5pp1/B2Rp3/1P6/6P1/4KP1P/8 b - - 0 1',
             nextMove: 'Rc8',
             playerColor: 'w',
             avantage: 'high'
         },
         {
-            title: 'Pion passé n°2',
+            title: 'Pion passé',
             startingFen: '4r2k/5PR1/p6p/P6P/3Nb3/8/1Pp5/2K5 b - - 0 1',
             nextMove: 'Rd8',
             playerColor: 'w',
             avantage: 'high'
         },
         {
-            title: 'Pion passé n°3',
+            title: 'Pion passé',
             startingFen: '8/8/3p4/p1nP4/k1P4P/6P1/8/7K w - - 0 38',
             nextMove: 'h5',
             playerColor: 'b',
             avantage: 'high'
         },
         {
-            title: 'Pion passé n°4',
+            title: 'Pion passé',
             startingFen: 'r6k/1pRN1pp1/4p2p/3rP2P/p7/4R3/5KP1/8 b - - 0 1',
             nextMove: 'Kh7',
             playerColor: 'w',
             avantage: 'medium'
+        },
+        {
+            title: 'Finale de tours',
+            startingFen: '8/5p2/6kp/2RN4/1p6/r2P1P2/P4KPP/8 b - - 0 27',
+            nextMove: 'Rxa2+',
+            playerColor: 'w',
+            avantage: 'very high'
         },
     ];
 
